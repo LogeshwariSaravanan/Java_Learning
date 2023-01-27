d24159d0b1d4b7505fceb3099009c5afe1f970e8
LogeshwariSaravanan <logeshwarisaravanan28@gmail.com>
2023-01-27 06:22:54 +0530

    added conditional operator

--- JAVA Basics/src/_12_TernaryOperator.java
+++ JAVA Basics/src/_12_TernaryOperator.java
@@ -1,18 +1,18 @@
 /*
-ternary operator -->condition ? exp1 : exp2;
+ternary operator or Conditional operator -->condition ? exp1 : exp2;
 check the condition if it is true print exp1 if it is false print exp2
  */
 import java.util.*;
 public class _12_TernaryOperator {
     public static void main(String[] args) {
-        int a,b;
+        int a,b,max;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of a: ");
         a=sc.nextInt();
         System.out.print("Enter the value of b: ");
         b=sc.nextInt();
-        int res= a>b?a:b;
-        System.out.println("greatest number is: "+res);
+        max= a>b?a:b;
+        System.out.println("greatest number is: "+max);
 
     }
 }
