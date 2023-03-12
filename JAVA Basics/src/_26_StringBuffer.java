e995bbb8365b52921edb91e46b51287b29ef1cea
LogeshwariSaravanan <logeshwarisaravanan28@gmail.com>
2023-03-12 19:33:05 +0530

    String buffer class

--- JAVA Basics/src/_26_StringBuffer.java
+++ JAVA Basics/src/_26_StringBuffer.java
@@ -4,9 +4,8 @@ public class _26_StringBuffer {
         System.out.println(s.append("JavaClass"));
         System.out.println(s.insert(7,"To"));
         System.out.println(s.delete(13,18));
-       // System.out.println(s.replace(1,3,"l"));
+        // System.out.println(s.replace(1,3,"l"));
         //System.out.println(s.reverse());
         System.out.println(s.length());
-
     }
 }
